0b5a6fedf773ad29c532f7a8ca53bfd0a8f78930
David <davidboxwell@ymail.com>
2023-02-22 12:52:24 +0000

    Debug break for linux

--- Smongine/src/Smongine/Core/Core.h
+++ Smongine/src/Smongine/Core/Core.h
@@ -2,9 +2,15 @@
 
 #define SM_ENABLE_ASSERTS // TODO: add to premake for debug configurations
 
+#ifdef SM_PLATFORM_WINDOWS
+	#define DEBUG_BREAK __debugbreak()
+#else
+	#define DEBUG_BREAK raise(SIGTRAP)
+#endif
+
 #ifdef SM_ENABLE_ASSERTS
-	#define SM_ASSERT(x, ...) if(!(x)) { SM_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); }
-	#define SM_CORE_ASSERT(x, ...) if(!(x)) { SM_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); }
+	#define SM_ASSERT(x, ...) if(!(x)) { SM_ERROR("Assertion Failed: {0}", __VA_ARGS__); DEBUG_BREAK; }
+	#define SM_CORE_ASSERT(x, ...) if(!(x)) { SM_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); DEBUG_BREAK; }
 #else
 	#define SM_ASSERT(x, ...) 
 	#define SM_CORE_ASSERT(x, ...)
